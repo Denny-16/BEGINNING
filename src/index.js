b8f2e6609108f175e5db92254eb320ea79e8b1d5
dennySatyala <denny.16satyala@gmail.com>
2025-06-14 10:17:54 +0530

    Update: Modified index.js

--- src/index.js
+++ src/index.js
@@ -4,8 +4,9 @@ import "./index.css"; // Tailwind styles
 
 function App() {
   return (
-    <div className="min-h-screen flex items-center justify-center bg-gradient-to-br from-blue-500 to-purple-600">
+    <div className="min-h-screen flex  items-center justify-center bg-gradient-to-br from-blue-500 to-purple-600">
       <h1 className="text-4xl font-bold text-white">Hello Tailwind + React + Parcel 🎉</h1>
+      <h2>what to do now??</h2>
     </div>
   );
 }
