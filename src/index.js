0f3c1ba6ecfda48d89c48cd44d0199b5bb9673ef
dennySatyala <denny.16satyala@gmail.com>
2025-06-14 11:31:13 +0530

    fifth commit

--- src/index.js
+++ src/index.js
@@ -4,10 +4,13 @@ import "./index.css"; // Tailwind styles
 
 function App() {
   return (
-    <div className="min-h-screen flex  items-center justify-center bg-gradient-to-br from-blue-500 to-purple-600">
+    <div className="min-h-screen flex items-center justify-center bg-gradient-to-br from-blue-500 to-purple-600">
       <h1 className="text-4xl font-bold text-white">Hello Tailwind + React + Parcel 🎉</h1>
+<<<<<<< HEAD
       <h2>what to do now??</h2>
       <p>This is denny From Visakhapatnam</p>
+=======
+>>>>>>> parent of b8f2e66 (Update: Modified index.js)
     </div>
   );
 }
